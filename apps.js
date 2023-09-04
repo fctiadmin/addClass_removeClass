364325f9f57a1e324070aca84af21133c90873a4
Future Computer Training Institute <107338637+fctiadmin@users.noreply.github.com>
2023-09-04 18:19:48 +0600

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,11 @@
+
+// function hide(){
+	// var h1= document.getElementById("heading");
+	// h1.style.display='none';
+// }
+
+function hide(){
+	
+	var h1= document.getElementById("heading");
+	h1.classList.toggle('show-hide');
+}
